88929e15509615fed28caa0fc6dca6dc581f7f13
Lou Morda <lmorda2@hotmail.com>
2025-01-20 17:30:13 -0800

    Removed unused imports

--- explore/src/main/java/com/lmorda/explore/GithubItemTitle.kt
+++ explore/src/main/java/com/lmorda/explore/GithubItemTitle.kt
@@ -1,6 +1,5 @@
 package com.lmorda.explore
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,7 +11,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
